b930340563f3f61e1939a82752871bd8563672b7
okabe007 <masaru@okabe.jp>
2025-06-08 11:32:48 +0900

    Codex パッチ適用

--- spermsim/main.py
+++ spermsim/main.py
@@ -123,7 +123,11 @@ def get_constants_from_gui(selected_data, shape, volume, sperm_conc):
     constants['surface_time']      = float(selected_data.get('surface_time', 0))
     constants['stick_sec']         = int(selected_data.get('stick_sec', 2))
     constants['stick_steps'] = constants['stick_sec'] * constants['sample_rate_hz']
+<<<<<<< ours
     constants['step_length'] = (constants['vsl'] * constants['sample_rate_hz']) / 1000
+=======
+    constants['step_length'] = constants['vsl'] / constants['sample_rate_hz'] if constants['sample_rate_hz'] else 0.0
+>>>>>>> theirs
     constants['limit'] = 1e-10
     egg_localization = selected_data.get('egg_localization', 'bottom_center').strip()
     constants['egg_localization'] = egg_localization
